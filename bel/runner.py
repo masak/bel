99e32b4e21fa70254e33c8a26e0e7afc6bbde3e2
Carl Masak <carl@masak.org>
2019-10-20 02:12:02 +0800

    Implement character syntax

--- bel/runner.py
+++ bel/runner.py
@@ -1,7 +1,17 @@
+class BelEvalError(Exception):
+    pass
+
+CHARCODES = set(["bel", "tab", "lf", "cr", "sp"])
+
 class Runner:
     def run(self, source):
         if source.startswith("'"):
             return source[1:]
+        elif source.startswith("\\"):
+            rest = source[1:]
+            if rest not in CHARCODES:
+                raise BelEvalError()
+            return source
         elif source == "":
             return None
         else:
---
+++ tests/test_character.py
@@ -0,0 +1,28 @@
+import unittest
+from bel.runner import Runner, BelEvalError
+
+runner = Runner()
+
+class Quote(unittest.TestCase):
+    def test_bel(self):
+        output = runner.run("\\bel")
+        self.assertEqual("\\bel", output)
+
+    def test_tab(self):
+        output = runner.run("\\tab")
+        self.assertEqual("\\tab", output)
+
+    def test_lf(self):
+        output = runner.run("\\lf")
+        self.assertEqual("\\lf", output)
+
+    def test_cr(self):
+        output = runner.run("\\cr")
+        self.assertEqual("\\cr", output)
+
+    def test_sp(self):
+        output = runner.run("\\sp")
+        self.assertEqual("\\sp", output)
+
+    def test_unknown_character(self):
+        self.assertRaises(BelEvalError, lambda: runner.run("\\turqois"))
