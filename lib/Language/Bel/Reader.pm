d87bfd8fdc221520d1eb2eb9bc14f56e6c9ea89e
Carl Masak <masak.carl.wilhelm@huawei.com>
2020-02-16 21:30:43 +0800

    Implement numbers in the reader

--- lib/Language/Bel/Reader.pm
+++ lib/Language/Bel/Reader.pm
@@ -15,6 +15,7 @@ use Language::Bel::Symbols::Common qw(
     SYMBOL_COMMA_AT
     SYMBOL_NIL
     SYMBOL_QUOTE
+    SYMBOL_T
 );
 use Exporter 'import';
 
@@ -218,7 +219,248 @@ sub maybe_wrap_in_no {
         ? make_symbol("no")
         : substr($part, 0, 1) eq "~"
             ? wrap_in_compose("no", substr($part, 1))
-            : make_symbol($part);
+            : make_number_or_symbol($part);
+}
+
+sub make_number_or_symbol {
+    my ($cs) = @_;
+
+    return parsenum($cs) || make_symbol($cs);
+}
+
+my $srzero = make_sr("+", 0, 1);
+my $srone = make_sr("+", 1, 1);
+my $srminusone = make_sr("-", 1, 1);
+
+# (def parsenum (cs base)
+#   (if (validi cs base)
+#       (buildnum srzero (parsei cs base))
+#       (let sign (check (car cs) signc)
+#         (let (ds es) (split signc (if sign (cdr cs) cs))
+#           (and (validr ds base)
+#                (or (no es) (validi es base))
+#                (buildnum (parsesr (consif sign ds) base)
+#                          (if (no es) srzero (parsei es base))))))))
+sub parsenum {
+    my ($cs) = @_;
+
+    if (validi($cs)) {
+        return buildnum($srzero, parsei($cs));
+    }
+    else {
+        my ($sign, $ds, $es) = $cs =~ /^([+\-]?+)([^+\-]*+)((?:[+\-][^+\-]*+)?+)$/
+            or return;
+        return validr($ds)
+            && ($es eq "" || validi($es))
+            && buildnum(parsesr("$sign$ds"), $es eq "" ? $srzero : parsei($es));
+    }
+}
+
+# (def validi (cs base)
+#   (and (signc (car cs))
+#        (= (last cs) \i)
+#        (let digs (cdr (dock cs))
+#          (or (no digs) (validr digs base)))))
+sub validi {
+    my ($cs) = @_;
+
+    return $cs =~ /^[+-](.*)i$/
+        && ($1 eq "" || validr($1));
+}
+
+# (def validr (cs base)
+#   (or (validd cs base)
+#       (let (n d) (split (is \/) cs)
+#         (and (validd n base)
+#              (validd (cdr d) base)))))
+#
+# (def validd (cs base)
+#   (and (all (cor [digit _ base] (is \.)) cs)
+#        (some [digit _ base] cs)
+#        (~cdr (keep (is \.) cs))))
+sub validr {
+    my ($cs) = @_;
+
+    my $validd = sub {
+        my ($cs) = @_;
+
+        return $cs =~ /^[\d\.]*+$/ && $cs =~ /\d/;
+    };
+
+    if ($validd->($cs)) {
+        return 1;
+    }
+    else {
+        my ($n, $d) = $cs =~ /^([^\/]*)\/([^\/]*)$/
+            or return;
+        return $validd->($n) && $validd->($d);
+    }
+}
+
+# (def parsei (cs base)
+#   (if (cddr cs)
+#       (parsesr (dock cs) base)
+#       (if (caris cs \+)
+#           srone
+#           (srinv srone))))
+sub parsei {
+    my ($cs) = @_;
+
+    return length($cs) > 2
+        ? parsesr(substr($cs, 0, -1))
+        : substr($cs, 0, 1) eq "+"
+            ? $srone
+            : $srminusone;
+}
+
+sub gcd {
+    my ($m, $n) = @_;
+
+    while ($m != 0 && $n != 0) {
+        if ($m > $n) {
+            $m %= $n;
+        }
+        else {
+            $n %= $m;
+        }
+    }
+
+    return $m > $n ? $m : $n;
+}
+
+# (def parsesr (cs base)
+#   (withs (sign  (if (signc (car cs)) (sym (list (car cs))))
+#           (n d) (split (is \/) (if sign (cdr cs) cs)))
+#     (simplify (cons (or sign '+)
+#                     (r/ (parsed n base)
+#                         (if d
+#                             (let rd (parsed (cdr d) base)
+#                               (if (caris rd i0)
+#                                   (err 'zero-denominator)
+#                                   rd))
+#                             (list i1 i1)))))))
+sub parsesr {
+    my ($cs) = @_;
+
+    my ($sign, $n, $d) = $cs =~ /^([+-]?+)([^\/]*+)((?:\/.*+)?)$/
+        or die "Due diligence -- this regex matches everything";
+    my $rn = parsed($n);
+    my $rd = [1, 1];
+    if ($d ne "") {
+        $rd = parsed(substr($d, 1));
+        if ($rd->[1] == 0) {
+            die "zero-denominator";
+        }
+    }
+
+    # (def simplify ((s n d))
+    #   (if (= n i0) (list '+ n i1)
+    #       (= n d)  (list s i1 i1)
+    #                (let g (apply i* ((of common factor) n d))
+    #                  (list s (car:i/ n g) (car:i/ d g)))))
+    my $simplify = sub {
+        my ($s, $n, $d) = @_;
+
+        if ($n == 0) {
+            return "+", $n, 1;
+        }
+        elsif ($n == $d) {
+            return $s, 1, 1;
+        }
+        else {
+            my $g = gcd($n, $d);
+            return $s, $n / $g, $d / $g;
+        }
+    };
+
+    my $r = [$rn->[0] * $rd->[1], $rd->[0] * $rn->[1]];
+    return make_sr($simplify->($sign || "+", $r->[0], $r->[1]));
+}
+
+# (set buildnum (of litnum simplify))
+sub buildnum {
+    my ($r, $i) = @_;
+
+    return make_number($r, $i);
+}
+
+# (def parsed (cs base)
+#   (let (i f) (split (is \.) cs)
+#     (if (cdr f)
+#         (list (parseint (rev (append i (cdr f))) base)
+#               (i^ base
+#                   (apply i+ (map (con i1) (cdr f)))))
+#         (list (parseint (rev i) base) i1))))
+sub parsed {
+    my ($cs) = @_;
+
+    my ($i, $f) = $cs =~ /(\d*)((?:\.\d+)?)/
+        or die "Due diligence -- this regex will never fail";
+    if (length($f) > 0) {
+        return [parseint(reverse($i . substr($f, 1))), 10 ** (length($f) - 1)];
+    }
+    else {
+        return [parseint(reverse($i)), 1];
+    }
+}
+
+# (def parseint (ds base)
+#   (if ds
+#       (i+ (charint (car ds))
+#           (i* base (parseint (cdr ds) base)))
+#       i0))
+sub parseint {
+    my ($ds) = @_;
+
+    my $result = 0;
+    for my $d (split //, $ds) {
+        $result *= 10;
+        $result += ord($d) - ord("0");
+    }
+    return $result;
+}
+
+sub make_number {
+    my ($r, $i) = @_;
+
+    return make_pair(
+        make_symbol("lit"),
+        make_pair(
+            make_symbol("num"),
+            make_pair(
+                $r,
+                make_pair(
+                    $i,
+                    SYMBOL_NIL,
+                ),
+            ),
+        ),
+    );
+}
+
+sub make_sr {
+    my ($sign, $n, $d) = @_;
+
+    return make_pair(
+        make_symbol($sign),
+        make_pair(
+            make_t_list($n),
+            make_pair(
+                make_t_list($d),
+                SYMBOL_NIL,
+            ),
+        ),
+    );
+}
+
+sub make_t_list {
+    my ($n) = @_;
+
+    my $list = SYMBOL_NIL;
+    for (1..$n) {
+        $list = make_pair(SYMBOL_T, $list);
+    }
+    return $list;
 }
 
 our @EXPORT_OK = qw(
--- t/number.t
+++ t/number.t
@@ -6,7 +6,7 @@ use Test::More;
 
 use Language::Bel;
 
-plan tests => 13;
+plan tests => 43;
 
 my $actual_output = "";
 my $b = Language::Bel->new({ output => sub {
@@ -37,4 +37,34 @@ sub is_bel_output {
     is_bel_output("(lit num (+ (t t) (t)) (- (t t t) (t)))", "2-3i");
     is_bel_output("(lit num (- (t t) (t)) (- (t t t) (t)))", "-2-3i");
     is_bel_output("(lit num (+ (t t t) (t t)) (+ (t) (t t t t)))", "3/2+1/4i");
+    is_bel_output("0", "0");
+    is_bel_output("1", "1");
+    is_bel_output("-1", "-1");
+    is_bel_output("5", "5");
+    is_bel_output("12", "12");
+    is_bel_output("+i", "+i");
+    is_bel_output("-i", "-i");
+    is_bel_output("+1i", "+i");
+    is_bel_output("-1i", "-i");
+    is_bel_output("+3i", "+3i");
+    is_bel_output("-4i", "-4i");
+    is_bel_output("0+0i", "0");
+    is_bel_output("0+i", "+i");
+    is_bel_output("0+1i", "+i");
+    is_bel_output("0-i", "-i");
+    is_bel_output("0-1i", "-i");
+    is_bel_output("1+i", "1+i");
+    is_bel_output("-2+1i", "-2+i");
+    is_bel_output("+3-2i", "3-2i");
+    is_bel_output("-4-i", "-4-i");
+    is_bel_output("1/2", "1/2");
+    is_bel_output("3/4-1/2i", "3/4-1/2i");
+    is_bel_output("2/4", "1/2");
+    is_bel_output("-3/9", "-1/3");
+    is_bel_output("0/5", "0");
+    is_bel_output("-0/3", "0");
+    is_bel_output("2.5", "5/2");
+    is_bel_output(".1", "1/10");
+    is_bel_output("2.5/3", "5/6");
+    is_bel_output("1.2/.3", "4");
 }
