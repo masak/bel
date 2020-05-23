d3c4f6b0be3b0dfcf7273149b2850e54ce6fde7e
Carl Masak <masak.carl.wilhelm@huawei.com>
2020-05-23 21:21:52 +0800

    Speed up r/

--- lib/Language/Bel/Globals.pm
+++ lib/Language/Bel/Globals.pm
@@ -1726,8 +1726,9 @@ $globals{"r*"} =
     SYMBOL_NIL))))), FASTFUNCS->{'r*'});
 
 $globals{"r/"} =
-    make_pair(make_symbol("lit"), make_pair(make_symbol("clo"),
-    make_pair(SYMBOL_NIL, make_pair(make_pair(make_pair(make_symbol("xn"),
+    make_fastfunc(make_pair(make_symbol("lit"),
+    make_pair(make_symbol("clo"), make_pair(SYMBOL_NIL,
+    make_pair(make_pair(make_pair(make_symbol("xn"),
     make_pair(make_symbol("xd"), SYMBOL_NIL)),
     make_pair(make_pair(make_symbol("yn"), make_pair(make_symbol("yd"),
     SYMBOL_NIL)), SYMBOL_NIL)), make_pair(make_pair(make_symbol("list"),
@@ -1735,7 +1736,7 @@ $globals{"r/"} =
     make_pair(make_symbol("yd"), SYMBOL_NIL))),
     make_pair(make_pair(make_symbol("i*"), make_pair(make_symbol("xd"),
     make_pair(make_symbol("yn"), SYMBOL_NIL))), SYMBOL_NIL))),
-    SYMBOL_NIL)))));
+    SYMBOL_NIL))))), FASTFUNCS->{'r/'});
 
 $globals{"srzero"} =
     make_pair(make_symbol("+"), make_pair(SYMBOL_NIL,
