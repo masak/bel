9d2cb2fa3993f30cfefc5bd31ad6b24fe2599108
Carl Masak <carl@masak.org>
2021-01-03 01:35:08 +0800

    Absorb L::B::T::Stream into L::B::Core

--- lib/Language/Bel/Core.pm
+++ lib/Language/Bel/Core.pm
@@ -28,13 +28,52 @@ sub xdr {
     return $self->{cdr} = $cdr;
 }
 
+package Language::Bel::Type::Stream;
+
+sub new {
+}
+
+sub read_char {
+    my ($self) = @_;
+
+    read($self->{handle}, my $chr, 1);
+    return $chr;
+}
+
+sub write_char {
+    my ($self, $chr) = @_;
+
+    die "'badmode\n"
+        unless $self->{mode} eq "out";
+
+    print {$self->{handle}} $chr;
+}
+
+sub stat {
+    my ($self) = @_;
+
+    return $self->{mode};
+}
+
+sub close {
+    my ($self) = @_;
+
+    die "'already-closed\n"
+        if $self->{mode} eq "closed";
+
+    close($self->{handle})
+        or die $!;
+    $self->{mode} = "closed";
+}
+
+sub mode {
+    my ($self) = @_;
+
+    return $self->{mode};
+}
+
 package Language::Bel::Core;
 
-use 5.006;
-use strict;
-use warnings;
-
-use Language::Bel::Type::Stream;
 use Language::Bel::Type::Symbol;
 
 use Exporter 'import';
@@ -144,7 +183,23 @@ sub make_stream {
 
     # XXX: error handle $mode values
 
-    return Language::Bel::Type::Stream->new($path_str, symbol_name($mode));
+    $mode = symbol_name($mode);
+
+    my $handle;
+    if ($mode eq "out") {
+        open($handle, ">", $path_str)
+            or die "'ioerror\n";
+    }
+    else {
+        open($handle, "<", $path_str)
+            or $! =~ /No such file/ and die "'notexist\n"
+            or die "'ioerror\n";
+    }
+
+    return bless(
+        { handle => $handle, mode => $mode },
+        "Language::Bel::Type::Stream",
+    );
 }
 
 sub make_symbol {
