0bb956657fdec09ab728d28b5159d747262a9461
Carl Masak <carl@masak.org>
2021-06-17 21:22:40 +0800

    Bump Bel version

--- lib/Language/Bel.pm
+++ lib/Language/Bel.pm
@@ -47,11 +47,11 @@ Language::Bel - An interpreter for Paul Graham's language Bel
 
 =head1 VERSION
 
-Version 0.54
+Version 0.55
 
 =cut
 
-our $VERSION = '0.54';
+our $VERSION = '0.55';
 
 =head1 SYNOPSIS
 
