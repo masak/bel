5ca013bca5861f22447580393674520439df1aea
Carl Masak <carl@masak.org>
2021-06-22 23:40:35 +0800

    Extract handling of primitives

--- lib/Language/Bel/Compiler.pm
+++ lib/Language/Bel/Compiler.pm
@@ -21,7 +21,9 @@ use Language::Bel::Pair::ByteFunc qw(
 use Language::Bel::Core qw(
     is_nil
     is_pair
+    is_symbol
     is_symbol_of_name
+    symbol_name
 );
 use Language::Bel::Primitives;
 use Language::Bel::Printer qw(
@@ -38,7 +40,7 @@ use Exporter 'import';
         output => sub {},
         read_char => sub {},
         err => sub {
-            die "Unexpected error during compilation";
+            die "`err` global unexpectedly call in the compiler";
         },
     });
 
@@ -55,8 +57,66 @@ use Exporter 'import';
     }
 }
 
+my %arg_count_of = (
+    "id" => 2,
+    "type" => 1,
+);
+
+sub is_primitive {
+    my ($op) = @_;
+
+    return
+        unless is_symbol($op);
+    my $name = symbol_name($op);
+    return !!$arg_count_of{$name};
+}
+
+sub handle_primitive {
+    my ($op, $instructions_ref, @args) = @_;
+
+    my $name = symbol_name($op);
+    my $expected_arg_count = $arg_count_of{$name};
+    my $actual_arg_count = scalar(@args);
+    die "Expected $expected_arg_count for primitive `$name`, "
+        . "got $actual_arg_count"
+        unless $actual_arg_count == $expected_arg_count;
+    handle_expression($args[0], $instructions_ref);
+    if ($name eq "id") {
+        my $symbol;
+        if (is_nil($args[1])) {
+            $symbol = SYMBOL("nil");
+        }
+        elsif (is_pair($args[1])
+            && is_symbol_of_name(car($args[1]), "quote")) {
+            my $quote_cadr = car(cdr($args[1]));
+            die "The quoted thing is not a symbol: ", _print($args[1])
+                unless is_symbol($quote_cadr);
+            my $symbol_name = symbol_name($quote_cadr);
+            $symbol = SYMBOL($symbol_name);
+        }
+        else {
+            die "Unexpected not-a-symbol";
+        }
+
+        push @{$instructions_ref}, (
+            SET_PRIM_ID_REG_SYM, 0, 0, $symbol,
+        );
+    }
+    elsif ($name eq "type") {
+        push @{$instructions_ref}, (
+            SET_PRIM_TYPE_REG, 0, 0, n,
+        );
+    }
+    else {
+        die "Unexpected primitive `$name`";
+    }
+}
+
 sub handle_expression {
     my ($expr, $instructions_ref) = @_;
+
+    return
+        unless is_pair($expr);
 
     my $op = car($expr);
     my $args = cdr($expr);
@@ -66,26 +126,8 @@ sub handle_expression {
         $args = cdr($args);
     }
 
-    if (is_symbol_of_name($op, "id")) {
-        if (scalar(@args) == 2
-            && is_symbol_of_name($args[0], "x")
-            && is_nil($args[1])) {
-            push @{$instructions_ref}, (
-                SET_PRIM_ID_REG_SYM, 0, 0, SYMBOL("nil"),
-            );
-        }
-        elsif (scalar(@args) == 2
-            && is_pair($args[0])
-            && is_pair($args[1])
-            && is_symbol_of_name(car($args[1]), "quote")) {
-            handle_expression($args[0], $instructions_ref);
-            push @{$instructions_ref}, (
-                SET_PRIM_ID_REG_SYM, 0, 0, SYMBOL("pair"),
-            );
-        }
-        else {
-            die "unexpected: ", _print($expr);
-        }
+    if (is_primitive($op)) {
+        handle_primitive($op, $instructions_ref, @args);
     }
     elsif (is_symbol_of_name($op, "no")) {
         if (scalar(@args) != 1) {
@@ -94,14 +136,6 @@ sub handle_expression {
         handle_expression($args[0], $instructions_ref);
         push @{$instructions_ref}, (
             SET_PRIM_ID_REG_SYM, 0, 0, SYMBOL("nil"),
-        );
-    }
-    elsif (is_symbol_of_name($op, "type")) {
-        if (scalar(@args) != 1) {
-            die "expected 1 arg: ", _print($expr);
-        }
-        push @{$instructions_ref}, (
-            SET_PRIM_TYPE_REG, 0, 0, n,
         );
     }
     else {
