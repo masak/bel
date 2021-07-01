1c7d8fa8258b4eb3eab5cf9950626f72832ef26e
Carl Masak <carl@masak.org>
2021-07-01 22:27:35 +0800

    Always return all_bytefuncs in order
    Instead of relying on hash key order, which in recent Perl
    releases is randomized for security reasons.

--- lib/Language/Bel/Globals/Bytecode.pm
+++ lib/Language/Bel/Globals/Bytecode.pm
@@ -22,29 +22,39 @@ use Language::Bel::Pair::ByteFunc qw(
 
 use Exporter 'import';
 
-my %bytefuncs = (
-    "no" => make_bytefunc(1, [
-        PARAM_IN, n, n, n,
-        SET_PARAM_NEXT, 0, n, n,
-        PARAM_LAST, n, n, n,
-        PARAM_OUT, n, n, n,
-        SET_PRIM_ID_REG_SYM, 0, 0, SYM_NIL,
-        RETURN_REG, 0, n, n,
-    ]),
-    "atom" => make_bytefunc(1, [
-        PARAM_IN, n, n, n,
-        SET_PARAM_NEXT, 0, n, n,
-        PARAM_LAST, n, n, n,
-        PARAM_OUT, n, n, n,
-        SET_PRIM_TYPE_REG, 0, 0, n,
-        SET_PRIM_ID_REG_SYM, 0, 0, SYM_PAIR,
-        SET_PRIM_ID_REG_SYM, 0, 0, SYM_NIL,
-        RETURN_REG, 0, n, n,
-    ]),
+my @all_bytefuncs;
+my %bytefuncs;
+
+sub add_bytefunc {
+    my ($name, $reg_count, @bytes) = @_;
+
+    my $bytefunc = make_bytefunc($reg_count, [@bytes]);
+    push @all_bytefuncs, $name;
+    $bytefuncs{$name} = $bytefunc;
+}
+
+add_bytefunc("no", 1,
+    PARAM_IN, n, n, n,
+    SET_PARAM_NEXT, 0, n, n,
+    PARAM_LAST, n, n, n,
+    PARAM_OUT, n, n, n,
+    SET_PRIM_ID_REG_SYM, 0, 0, SYM_NIL,
+    RETURN_REG, 0, n, n,
+);
+
+add_bytefunc("atom", 1,
+    PARAM_IN, n, n, n,
+    SET_PARAM_NEXT, 0, n, n,
+    PARAM_LAST, n, n, n,
+    PARAM_OUT, n, n, n,
+    SET_PRIM_TYPE_REG, 0, 0, n,
+    SET_PRIM_ID_REG_SYM, 0, 0, SYM_PAIR,
+    SET_PRIM_ID_REG_SYM, 0, 0, SYM_NIL,
+    RETURN_REG, 0, n, n,
 );
 
 sub all_bytefuncs {
-    return keys(%bytefuncs);
+    return @all_bytefuncs;
 }
 
 sub bytefunc {
