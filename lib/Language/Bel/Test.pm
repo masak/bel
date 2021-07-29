b67d1d1293dba89e2f9f05a6814acf537e4f407f
Carl Masak <carl@masak.org>
2021-07-29 23:57:17 +0800

    Remove unused function 'bel_todo'

--- lib/Language/Bel/Test.pm
+++ lib/Language/Bel/Test.pm
@@ -19,49 +19,6 @@ use Language::Bel::Compiler qw(
 
 use Exporter 'import';
 
-my $actual_output = "";
-my $prepared_input = "Bel 9 from Outer Space\n"; 
-my $b = Language::Bel->new({
-    output => sub {
-        my ($string) = @_;
-        $actual_output = "$actual_output$string";
-    },
-    read_char => sub {
-        my $chr = substr($prepared_input, 0, 1);
-        $prepared_input = substr($prepared_input, 1);
-        return $chr;
-    },
-});
-
-sub bel_todo {
-    my ($expr, $expected_output, $expected_todo_error) = @_;
-
-    $expected_todo_error ||= "";
-    $actual_output = "";
-    eval {
-        $b->read_eval_print($expr);
-    };
-    $actual_output =~ s/\r?\n$//;
-
-    my $error = $@;
-    $error =~ s/\n$//;
-    if (!$error && $actual_output eq $expected_output) {
-        ok(0, "UNEXPECTED SUCCESS $expr ==> $expected_output");
-    }
-    else {
-        my $message = $error eq $expected_todo_error
-            ? "'$error'"
-            : $error
-                ? "Expected '$expected_todo_error', got '$error'"
-                : "Expected '$expected_output', got '$actual_output'";
-        ok(
-            $expected_todo_error && $error && $error eq $expected_todo_error
-            || !$expected_todo_error && !$error && $actual_output ne $expected_output,
-            "TODO $expr ($message)"
-        );
-    }
-}
-
 sub visit {
     my ($dir, $fn_ref, $prefix) = @_;
     $prefix ||= "";
@@ -174,7 +131,6 @@ sub test_compilation {
 }
 
 our @EXPORT = qw(
-    bel_todo
     for_each_line_in_file
     output_of_eval_file
     slurp_file
